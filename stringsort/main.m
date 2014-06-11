2956f46b50050f527ee6a6c38b4a65530b80705a
Colin <appdev-ios@audi.com.cn>
2014-06-11 14:34:26 +0800

    Initial Commit

---
+++ stringsort/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  stringsort
+//
+//  Created by Colin on 14-6-11.
+//  Copyright (c) 2014年 colin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
